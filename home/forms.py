13d0da77a190e87da96de871ccda3ea512c279d1
leprikon8888 <leprikon8888@gmail.com>
2024-02-19 17:12:07 +0200

    вроде все

---
+++ home/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import FormContact
+
+
+class FormContactForm(forms.ModelForm):
+    class Meta:
+        model = FormContact
+        fields = ('name', 'email', 'phone', 'message')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': "form-control", 'id': "name", 'placeholder': "Your Name",
+                                           'required': True }),
+            'email': forms.EmailInput(attrs={ 'class': "form-control", 'id': "email", 'placeholder': "Your Email",
+                                              'required': True}),
+            'phone': forms.TextInput(attrs={'class': "form-control", 'placeholder': "Your Phone", 'required': True}),
+            'message': forms.Textarea(attrs={'class': "form-control", 'id': "message", 'placeholder': "Your Message",
+                                             'required': True})
+        }
